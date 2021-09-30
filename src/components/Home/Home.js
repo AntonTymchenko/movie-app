7d772c91b6505a1185b2f48994f6b96853f769a7
AntonTymchenko <76427403+AntonTymchenko@users.noreply.github.com>
2021-09-30 22:20:41 +0300

    movie app
    search movie and shows by id

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,23 +1,17 @@
 import React, { useEffect } from "react";
-import { MovieListing } from "../MovieListing/MovieListing";
-import movieApi from "../../common/apis/movieApi";
-import { APIKey } from "../../common/apis/MovieApiKey";
 import { useDispatch } from "react-redux";
-import { addMovies } from "../../redux/movies/movieSlice";
+import { MovieListing } from "../MovieListing/MovieListing";
+import {
+  fetchAsyncMovies,
+  fetchAsyncShows,
+} from "../../redux/movies/movieSlice";
 
 const Home = () => {
-  const movieText = "Harry";
   const dispatch = useDispatch();
-
   useEffect(() => {
-    const fetchMovies = async () => {
-      const response = await movieApi
-        .get(`?apiKey=${APIKey}&s=${movieText}&type=movie`)
-        .catch((err) => console.log(err));
-      dispatch(addMovies(response.data));
-    };
-    fetchMovies();
-  }, []);
+    dispatch(fetchAsyncMovies());
+    dispatch(fetchAsyncShows());
+  }, [dispatch]);
   return (
     <div>
       <div className="banner-img"></div>
--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getAllMovies } from "../../redux/movies/movieSlice";
+import { getAllMovies, getAllShows } from "../../redux/movies/movieSlice";
 import { MovieCard } from "../MovieCard/MovieCard";
 import "./MovieListing.scss";
 
 const MovieListing = () => {
   const movies = useSelector(getAllMovies);
+  const shows = useSelector(getAllShows);
 
   return (
     <div className="movie-wrapper">
@@ -13,11 +14,25 @@ const MovieListing = () => {
         <h2>Movies</h2>
         <div className="movie-container">
           {movies.Response === "True" ? (
-            movies.Search.map((movie, index) => {
-              return <MovieCard key={index} data={movie} />;
+            movies.Search.map((movie) => {
+              return <MovieCard key={movie.imdbID} data={movie} />;
             })
           ) : (
             <div className="movies-error">
+              <h3>{movies.Error}</h3>
+            </div>
+          )}
+        </div>
+      </div>
+      <div className="show-list">
+        <h2>Shows</h2>
+        <div className="movie-container">
+          {shows.Response === "True" ? (
+            shows.Search.map((show, index) => {
+              return <MovieCard key={index} data={show} />;
+            })
+          ) : (
+            <div className="shows-error">
               <h3>{movies.Error}</h3>
             </div>
           )}
